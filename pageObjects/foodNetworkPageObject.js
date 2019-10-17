3e12ec06813396268287e0f5434320318d6ea8ff
danielpeng95 <danielpeng95@yahoo.com>
2019-10-17 15:21:23 -0600

    update

--- pageObjects/foodNetworkPageObject.js
+++ pageObjects/foodNetworkPageObject.js
@@ -1,18 +1,59 @@
 module.exports = {
     url: 'https://www.foodnetwork.com',
-    
-    elements: { 
+    commands: [{
+        clickVerify: function (selector, selector2, text) {
+            this
+                .waitForElementVisible(selector, 20000)
+                .click(selector)
+                .waitForElementVisible(selector2, 20000)
+                .verify.containsText(selector2, text)
+            return this
+        },
+        searchVerify: function (selector, selector2, text) {
+            this
+                .waitForElementVisible(selector, 20000)
+                .setValue(selector, [text, this.api.Keys.ENTER]) //make sure you have "this.api." when you are in custom commands"
+                .waitForElementVisible(selector2, 20000)
+                .verify.containsText(selector2, text)
+            return this
+        },
+        scrollDownBy: function (pixels) { //custom command for scrolling down
+            this.api.execute(`window.scrollBy(0, ${pixels})`)
+            return this
+        },
+
+    }],
+    elements: {
 
         //log in log out
         burger: '[class="a-Icon a-Icon--menu o-HeaderFresh__a-Icon o-HeaderFresh__a-Icon--menu"]',
         logIn: '[class="m-ProfileInfo__a-Button--Login"]',
-        email: '[class="gigya-input-text gigya-valid"]',
-        // email: {selector: '(//input[@name=“username”])[1]', locateStrategy: 'xpath'},
-        password: '[class="gigya-input-password gigya-valid"]',
-        submit: {selector: '(//input[@data-screenset-element-id="__gig_template_element_10_1571334231948"])[1]', locateStrategy: 'xpath'},
+        email: { selector: '(//input[@data-gigya-name="loginID"])[1]', locateStrategy: 'xpath' },
+        password: { selector: '(//input[@data-gigya-name="password"])[1]', locateStrategy: 'xpath' },
+        submit: { selector: '(//input[@class="gigya-input-submit"])[1]', locateStrategy: 'xpath' },
         nickName: '[class="m-ProfileInfo__a-Nickname"]',
 
+        //Categories
+        header: '[class="o-AssetTitle__a-HeadlineText"]',
+        body: '[class="container-site is-Fluid"]',
+        body2: '[class="container-site "]',
+        recipes: { selector: '(//a[@href="//www.foodnetwork.com/recipes"])[1]', locateStrategy: 'xpath' },
+        healthy: { selector: '(//a[@href="//www.foodnetwork.com/healthy"])[1]', locateStrategy: 'xpath' },
+        party: { selector: '(//a[@href="//www.foodnetwork.com/holidays-and-parties"])[1]', locateStrategy: 'xpath' },
+        halloween: { selector: '(//a[@href="//www.foodnetwork.com/holidays-and-parties/packages/halloween"])[1]', locateStrategy: 'xpath' },
+        shop: { selector: '(//a[@href="//www.foodnetwork.com/how-to/packages/shopping"])[1]', locateStrategy: 'xpath' },
+        shows: { selector: '(//a[@href="//www.foodnetwork.com/shows"])[1]', locateStrategy: 'xpath' },
+        baking: { selector: '(//a[@href="//www.foodnetwork.com/shows/halloween-baking-championship"])[1]', locateStrategy: 'xpath' },
+        schedule: { selector: '(//a[@href="//www.foodnetwork.com/shows/tv-schedule"])[1]', locateStrategy: 'xpath' },
+
+        //search function
+        search: '[id="searchHeader"]',
+        result: '[class="siteSearchResults"]',
+
+        //Get Dinner/Input Search/Write File
+        dinner: '[class="recipes_link"]',
+        firstRes: { selector: '(//span[@class="m-MediaBlock__a-HeadlineText"])[1]', locateStrategy: 'xpath' },
+        ingredients: '[class="recipe-body"]',
 
     }
-    
 }
--- tests/foodNetwork.js
+++ tests/foodNetwork.js
@@ -1,14 +1,16 @@
-var food
+var fs = require("fs")
+var Food
 module.exports = {
     beforeEach: browser => {
-        food = browser.page.foodNetworkPageObject()
-        food.navigate()
+        Food = browser.page.foodNetworkPageObject()
+        Food.navigate()
+        Food.api.maximizeWindow()
     },
     after: browser => {
-        food.end()
+        Food.end()
     },
     'log in log out': browser => {
-        food
+        Food
             .waitForElementVisible('@burger', 20000)
             .click('@burger')
             .waitForElementVisible('@logIn', 20000)
@@ -17,6 +19,57 @@ module.exports = {
             .setValue('@email', 'softwareqa10@yahoo.com')
             .setValue('@password', 'SoftQA1995')
             .click('@submit')
-            .verify.containsText('@nickName', 'softwareqa')
-    }
+            .verify.containsText('@nickName', 'SOFTWAREQA')
+    },
+    'Categories': browser => {
+        Food.clickVerify('@recipes', '@header', 'Recipes')
+        Food.clickVerify('@healthy', '@header', 'Healthy')
+        Food.clickVerify('@party', '@header', 'Party')
+        Food.clickVerify('@halloween', '@body', 'Halloween')
+        Food.clickVerify('@shop', '@body', 'SHOP')
+        Food.clickVerify('@shows', '@header', 'Shows')
+        Food.clickVerify('@baking', '@body2', 'Baking')
+        Food.clickVerify('@schedule', '@header', 'Schedule')
+    },
+    'Search Function': browser => {
+        Food.searchVerify('@search', '@result', 'Chicken')
+    },
+    'Get Dinner/Input Search/write file': browser => {
+        var originalWindow = ""
+        var newWindow = ""
+        var dinner = ""
+
+        Food.api.windowHandles(function (result) {
+            originalWindow = result.value[0]
+        })
+        Food.api.openNewWindow()
+        Food.api.windowHandles(function (result) {
+            newWindow = result.value[1]
+            Food.api.switchWindow(newWindow)
+        })
+        Food.api.url('https://randomwordgenerator.com/dinner-ideas.php')
+
+        Food.waitForElementVisible('@dinner', 20000)
+            .getText('@dinner', function (result) {
+                dinner = result.value
+                console.log(dinner)
+            })
+        Food.api.windowHandles(function (result) {
+            Food.api.switchWindow(originalWindow)
+        })
+        // Food.perform(() => Food.searchVerify('@search', '@result', dinner))
+        Food
+            .waitForElementVisible('@search')
+            .perform(() =>
+                Food.setValue('@search', [dinner, browser.Keys.ENTER])
+            )
+        Food
+            .waitForElementVisible('@firstRes', 20000)
+            .click('@firstRes')
+            .waitForElementVisible('@ingredients', 20000)
+            .getText('@ingredients', function (result) {
+                var ing = result.value
+                fs.writeFileSync('./testAssets/ingredients.txt', ing)
+            })
+    },
 }
