d1ec1a1db59515ef29bf1f491c4de0371d6dfa55
danielpeng95 <danielpeng95@yahoo.com>
2019-10-17 11:54:24 -0600

    update

--- pageObjects/foodNetworkPageObject.js
+++ pageObjects/foodNetworkPageObject.js
@@ -11,26 +11,7 @@ module.exports = {
         submit: {selector: '(//input[@data-screenset-element-id="__gig_template_element_10_1571334231948"])[1]', locatestrategy: 'xpath'},
         nickName: '[class="m-ProfileInfo__a-Nickname"]',
 
-        
+
     }
-
-
-    // elements: { //another way to write CSS selector
-    //     searchBar: '[type="text"]',
-    //     resultCity: '.current-weather__location',
-    //     errorMessage: '.error-message__message',
-    //     searchButton: '.enter-location__submit',
-    //     searchAgainButton: '.current-weather__search-again',
-    //     tryAgainButton: '.error-message__try-again'
-    // }
-
-    // elements: { //xPath Selector
-    //     searchBar: {selector: '//input[@type="text"]', locateStrategy: 'xpath'},
-    //     resultCity: {selector: '//h3[@class="current-weather__location"]', locateStrategy: 'xpath'},
-    //     errorMessage: {selector: '//h3[@class="error-message__message"]', locateStrategy: 'xpath'},
-    //     searchButton: {selector: '//button[@class="enter-location__submit"]', locateStrategy: 'xpath'},
-    //     searchAgainButton: {selector: '//button[@class="current-weather__search-again"]', locateStrategy: 'xpath'},
-    //     tryAgainButton: {selector: '//button[@class="error-message__try-again"]', locateStrategy: 'xpath'}
-    // }
     
 }
