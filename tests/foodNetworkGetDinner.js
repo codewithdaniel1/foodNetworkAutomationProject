349c3213bb9ac605e6c6544c09787373085fd44b
danielpeng95 <danielpeng95@yahoo.com>
2019-10-21 15:50:32 -0600

    update

--- tests/foodNetworkGetDinner.js
+++ tests/foodNetworkGetDinner.js
@@ -24,6 +24,7 @@ module.exports = {
         })
 
         Food.api.openNewWindow()
+        
         Food.api.windowHandles(function (result) {
             newWindow = result.value[1]
             Food.api.switchWindow(newWindow)
