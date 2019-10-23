fcd611950040b491e75ce3124e3459a3ae876206
danielpeng95 <danielpeng95@yahoo.com>
2019-10-22 19:58:41 -0600

    update

--- nightwatch.props.js
+++
@@ -1,9 +0,0 @@
-
-
-module.exports = {
-    resourcePath: "C:\\Users\\benpe\\DevMountain\\testing-resources\\",
-    seleniumServer: "selenium-server-standalone-3.141.59.jar",
-    chromedriver: "chromedriver.exe"
- }
-
- 
